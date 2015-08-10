6713ae576bbb3f3e53749c1d946de945560ed28e
Howl <git@howl.moe>
2015-08-10 12:44:59 +0200

    2spaces >>>>> 4spaces

--- index.js
+++ index.js
@@ -10,29 +10,29 @@ var mirror = require("./mirror")
 console.log("finished loading.")
 
 var loginObject = { 
-    username: config.user, 
-    password: config.password, 
-    autologin: "on", 
-    sid: "",
-    redirect: "index.php",
-    viewonline: "off",
-    login: "Login"
+  username: config.user, 
+  password: config.password, 
+  autologin: "on", 
+  sid: "",
+  redirect: "index.php",
+  viewonline: "off",
+  login: "Login"
 }
 
 console.log("logging in...")
 request.post({ url: "https://osu.ppy.sh/forum/ucp.php?mode=login", form: loginObject }, function(err,httpResponse,body) {
-    if (err === null)
-        console.log("Page downloading success!")
-    else
-        throw ""
+  if (err === null)
+    console.log("Page downloading success!")
+  else
+    throw ""
 
-    console.log("now downloading test beatmap...")
-    request("https://osu.ppy.sh/d/194239n", function() {
-        console.log("done. checking download worked...")
-        if (!ibf("maps/test/test-beatmap.osz")) {
-            console.log("Something went wrong. The test beatmap isn't a binary file. Perhaps you wrote the wrong password?")
-        }
-        console.log("It seems to have worked! Now starting.")
-        mirror.mirrorStart(fs, request, config, ibf)
-    }).pipe(fs.createWriteStream("maps/test/test-beatmap.osz"))
+  console.log("now downloading test beatmap...")
+  request("https://osu.ppy.sh/d/194239n", function() {
+    console.log("done. checking download worked...")
+    if (!ibf("maps/test/test-beatmap.osz")) {
+      console.log("Something went wrong. The test beatmap isn't a binary file. Perhaps you wrote the wrong password?")
+    }
+    console.log("It seems to have worked! Now starting.")
+    mirror.mirrorStart(fs, request, config, ibf)
+  }).pipe(fs.createWriteStream("maps/test/test-beatmap.osz"))
 })
