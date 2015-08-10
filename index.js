74271f6e4a7c6608e2803185761b93f28bea8a74
Howl <git@howl.moe>
2015-08-10 12:48:06 +0200

    Add some comments to index.js, for readability's sake.

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var mirror = require("./mirror")
 
 console.log("finished loading.")
 
+// On https://osu.ppy.sh/forum/ucp.php?mode=login there is a form, that has these elements.
+// This object essentially contains the data to send to the osu! server for logging in.
 var loginObject = { 
   username: config.user, 
   password: config.password, 
@@ -20,6 +22,7 @@ var loginObject = {
 }
 
 console.log("logging in...")
+// Making a POST request to the URL https://osu.ppy.sh/forum/ucp.php?mode=login, sending the data in loginObject.
 request.post({ url: "https://osu.ppy.sh/forum/ucp.php?mode=login", form: loginObject }, function(err,httpResponse,body) {
   if (err === null)
     console.log("Page downloading success!")
